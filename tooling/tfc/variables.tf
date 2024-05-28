819cfec868079ab48ec68203fa321146100dcfbb
Declan Morris <dtpmorris@gmail.com>
2024-05-28 14:08:03 +0100

    added structure for tfc

---
+++ tooling/tfc/variables.tf
@@ -0,0 +1,63 @@
+variable "projects" {
+  type = map(object({
+    name = string
+    path = string
+    workspace = string
+  }))
+  default = {
+    "project-alpha-prod" = {
+      name = "project-alpha"
+      path = "/terraform/project-alpha"
+      workspace = "prod"
+    },
+    "project-alpha-dev" = {
+      name = "project-alpha"
+      path = "/terraform/project-alpha"
+      workspace = "dev"
+    },
+    "project-omega-prod" = {
+      name = "project-omega"
+      path = "/terraform/project-omega"
+      workspace = "prod"
+    },
+    "project-omega-dev" = {
+      name = "project-omega"
+      path = "/terraform/project-omega"
+      workspace = "dev"
+    }
+  }
+}
+
+locals {
+  workspaces = distinct([for k, project in var.projects : project.workspace ])
+}
+
+output "workspaces" {
+  value = local.workspaces
+}
+
+variable "aws_access_key" {
+  type = string
+  description = "AWS access key"
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type = string
+  description = "AWS secret key"
+  sensitive = true
+}
+
+variable "organization" {
+  type = string
+  default = ""
+}
+
+variable "github_token" {
+  sensitive = true
+}
+
+variable "email" {
+  type = string
+  sensitive = true
+}
